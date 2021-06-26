dcde34cb457b4a4f3182c3acde1abb3d2c7c98ee
unknown <karmasumit7@gmail.com>
2021-06-26 09:18:05 +0530

    ExecutorFramework: Write example for singleThreadExecutor and FixedScheduledThreadPool

---
+++ src/main/java/concepts/threads/executors/FixedThreadPoolExecutorExample.java
@@ -0,0 +1,44 @@
+package concepts.threads.executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class FixedThreadPoolExecutorExample extends Thread {
+
+    int i;
+
+    public FixedThreadPoolExecutorExample(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public void run() {
+
+        long duration = (long) (Math.random()*5);
+
+        System.out.println("Doing some job by Thread "+ i + " Thread id: " + Thread.currentThread().getName());
+
+        try {
+            TimeUnit.SECONDS.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 10 ; i++) {
+
+            executorService.execute(new FixedThreadPoolExecutorExample(i));
+
+        }
+
+
+
+    }
+}
